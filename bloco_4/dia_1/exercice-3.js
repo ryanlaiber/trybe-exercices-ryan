9da5ad51ee46da3950eae371435c8e29a21aae48
RyanLaiber <ryanlaiber@gmail.com>
2021-04-22 18:16:18 -0300

    adiciona exercicio 3

---
+++ bloco_4/dia_1/exercice-3.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 6;
+let c = 7;
+
+if (a < b){
+    if (b < c){
+        console.log(c);
+    }
+    else{
+        console.log(b);
+    }
+    
+}
+else{
+    console.log(a);
+}
