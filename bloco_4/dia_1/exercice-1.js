ba14b173bace8bb4c9818a9b929d632e175408cf
RyanLaiber <ryanlaiber@gmail.com>
2021-04-22 18:11:01 -0300

    adiciona exercicio 2

--- bloco_4/dia_1/exercice-1.js
+++ bloco_4/dia_1/exercice-1.js
@@ -1,8 +1,8 @@
 let a = 5;
 let b = 6;
 
-console.log("1.1: "+ (a + b))
-console.log("1.2: "+ (a - b))
-console.log("1.3: "+ (a * b))
-console.log("1.4: "+ (a / b))
-console.log("1.5: "+ (a % b))
+console.log("1.1: "+ (a + b));
+console.log("1.2: "+ (a - b));
+console.log("1.3: "+ (a * b));
+console.log("1.4: "+ (a / b));
+console.log("1.5: "+ (a % b));
---
+++ bloco_4/dia_1/exercice-2.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 6;
+
+if (a < b){
+    console.log(b);
+}
+else{
+    console.log(a);
+}
