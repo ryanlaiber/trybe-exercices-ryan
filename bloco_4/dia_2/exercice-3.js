7ba75e6de05770ce9a3dd66079d184f4e1987bab
RyanLaiber <ryanlaiber@gmail.com>
2021-04-23 18:31:50 -0300

    adiciona exercício 3

---
+++ bloco_4/dia_2/exercice-3.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let som = 0
+
+for(let number = 0; number < numbers.length; number += 1){
+    som = som + numbers[number];
+};
+
+console.log(som / numbers.length);
