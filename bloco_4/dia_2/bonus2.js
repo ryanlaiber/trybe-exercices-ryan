96b4600165dd0a0069123392bbd53c817aca565f
RyanLaiber <ryanlaiber@gmail.com>
2021-04-23 19:25:08 -0300

    adiciona exercício bonus 2

--- bloco_4/dia_2/bonus2.js
+++ bloco_4/dia_2/bonus2.js
@@ -1,9 +1,9 @@
-//orderm crescente
+// ordem decrescente
 let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 
 for (let index = 1; index < numbers.length; index ++){
     for (let index2 = 0; index2 < numbers.length; index2 ++){
-        if (numbers[index2] > numbers[index]){
+        if (numbers[index2] < numbers[index]){
             let position = numbers[index2];
             numbers[index2] = numbers[index];
             numbers[index] = position;
