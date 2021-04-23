2a84a0c4f8983a4dc4783888f186b827a2612402
RyanLaiber <ryanlaiber@gmail.com>
2021-04-23 19:42:59 -0300

    adiciona exercício bonus 3

---
+++ bloco_4/dia_2/bonus3.js
@@ -0,0 +1,13 @@
+// multiplica um número da liste pelo número seguinte
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let mult = [];
+
+for (let index = 0; index < numbers.length; index ++){
+    if (index < (numbers.length -1)){
+        mult.push(numbers[index] * numbers[index + 1]);
+    }else{
+        mult.push(numbers[index] * 2);
+    }
+}
+
+console.log(mult);
