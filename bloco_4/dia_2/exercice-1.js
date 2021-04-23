91364483609a70bf299dc79e26698189816751d6
RyanLaiber <ryanlaiber@gmail.com>
2021-04-23 18:24:09 -0300

    adiciona exercício 1

--- bloco_4/dia_2/exercice-1.js
+++ bloco_4/dia_2/exercice-1.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let number; number < numbers.length(); number ++){
-    console.log(numbers[number]);
-};
+for(number of numbers){
+    console.log(number);
+}
