3d042fc584d378d1cc8c496909287b3a305a588d
RyanLaiber <ryanlaiber@gmail.com>
2021-04-23 19:23:26 -0300

    adiciona exercício bonus 1

---
+++ bloco_4/dia_2/bonus.js
@@ -0,0 +1,14 @@
+//orderm crescente
+let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
+
+for (let index = 1; index < numbers.length; index ++){
+    for (let index2 = 0; index2 < numbers.length; index2 ++){
+        if (numbers[index2] > numbers[index]){
+            let position = numbers[index2];
+            numbers[index2] = numbers[index];
+            numbers[index] = position;
+        }
+    }
+}
+
+console.log(numbers);
