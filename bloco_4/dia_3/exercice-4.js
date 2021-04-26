8c803fedc8f1c775afe16325097191f6ff5f98ee
RyanLaiber <ryanlaiber@gmail.com>
2021-04-26 20:22:38 -0300

    adiciona exercício 4 parte 1

---
+++ bloco_4/dia_3/exercice-4.js
@@ -0,0 +1,29 @@
+let n = 13;
+let t = "*";
+let s = " ";
+let square = "";
+
+for (let l = 0; l < n; l += 1){
+
+    if (square.length == 0){
+
+        for (let c = 0; c < n; c += 1){
+            square = square + t;
+        }
+        console.log(square); 
+
+    }else if (square.length > 0){
+
+        square = "";
+        let o = n - (2 * l) ;
+        let le = (n - l) - o;
+        for (let spaces = 0; spaces < le; spaces += 1){
+            square = square + s;
+        };
+        for (let dots = 0; dots < o; dots += 1){
+            square = square + t;
+        };
+        
+        console.log(square);
+        }
+    }
