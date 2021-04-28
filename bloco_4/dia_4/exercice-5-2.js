bc8808f1851962431ae1e6c1ae4b808005ba81d6
RyanLaiber <ryanlaiber@gmail.com>
2021-04-28 13:49:32 -0300

    adiciona exercício 5.2 dia 4

--- bloco_4/dia_4/exercice-5-2.js
+++ bloco_4/dia_4/exercice-5-2.js
@@ -4,24 +4,23 @@ function maisRepete(lista){
         return console.log("Não é lista");
      };
     
-    let counterCheck = 0;
-    let popular = lista[0];
+    let counterCheck = 0;// armazena a quantidade de repetições  do mais popular
+    let popular = lista[0];// armazena o valor do mais popular
 
     for(let number of lista){
-        let counter = 0;
-        let sameNumber = number;
+        let counter = 0;//contador de repetições 
         for(let compareNumber of lista){
-            if(compareNumber === sameNumber){
+            if(compareNumber === number){
                 counter += 1;
-            };
+            };//conta as repetições
         };
         if(counter > counterCheck){
             counterCheck = counter;
             popular = number;
-        };
+        };//checa se number é o mais popular e atribui valor para popular
     };
 
     return console.log(popular);
 };
 
-maisRepete([3,2,5,2,3,6,5,8,5,9]);
+maisRepete([3,2,5,2,3,3,5,5,5,9]);
