0d043405ddf6c425a2cb4f0fce6f941e32f5dfdf
RyanLaiber <ryanlaiber@gmail.com>
2021-05-11 19:12:32 -0300

    add first request, add personal data

---
+++ bloco_6/dia_1/script.js
@@ -0,0 +1,51 @@
+//  caminhos
+let caminhoEstadosInput = document.getElementById("estados");
+
+
+//  função adiciona option estados no id:estados
+let estadosBR = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+};
+
+function adicionaEstados (objeto){
+    for (let key in objeto){
+        let optionEstado = document.createElement("option");
+        optionEstado.value = key;
+        optionEstado.innerText = objeto[key];
+        caminhoEstadosInput.appendChild(optionEstado);
+    };
+};
+
+
+
+
+//  carrega as funções
+window.onload = function (){
+    adicionaEstados(estadosBR);
+};
