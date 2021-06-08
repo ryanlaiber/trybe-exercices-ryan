05850426fbede0da4664774b3627e141b7bcd399
RyanLaiber <ryanlaiber@gmail.com>
2021-06-08 19:37:19 -0300

    adiciona exercicio dia 2

--- bloco_9/dia_2-promises/exercices/apiScript.js
+++ bloco_9/dia_2-promises/exercices/apiScript.js
@@ -3,6 +3,19 @@ const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
   // Adicionar lógica aqui!
+  const reqJson = {
+    method: 'GET',
+    headers: {'Accept': 'application/json'}
+  };
+  fetch(API_URL, reqJson)
+    .then(response => response.json())
+    .then(response => document.querySelector('h1').innerHTML = response.joke);
+
+  return new Promise((resolve, reject) => {
+    const arr = Array.from({ length: 10 },
+      () => Math.floor(Math.random() * 51));
+    (arr[0] > 20) ? resolve(arr) : reject('oioi');
+  }).then(response => console.log(response[0])).catch(msg => console.log(msg));
 };
 
 window.onload = () => fetchJoke();
