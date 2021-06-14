753e601f9b072fe4a5e21a2954e266487ebc8f6d
RyanLaiber <ryanlaiber@gmail.com>
2021-06-14 20:00:18 -0300

    adiciona testes de hydrate.js

--- bloco_10/dia_1-jest/hydrate.test.js
+++ bloco_10/dia_1-jest/hydrate.test.js
@@ -1,2 +1,12 @@
 const {hydrate} = require('./hydrate');
+const {test, describe, expect, toThrow, toBe} = require('@jest/globals');
 
+describe('função hydrate:', () => {
+    test('não é string', () => {
+        expect(() => {hydrate(5)}).toThrow();
+    });
+    test('soma certinho:', () => {
+        expect(hydrate('2 cervejas, 50 latinhas de gasolina e 20 vódcas')).toBe('72 copos de água');
+        expect(hydrate('1 trem')).toBe('1 copo de água');
+    });
+});
