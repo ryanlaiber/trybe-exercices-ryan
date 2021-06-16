e7c91213c67efef50c794f2505d24d2d8789e153
RyanLaiber <ryanlaiber@gmail.com>
2021-06-16 20:38:11 -0300

    termina o dia com duvida sobre o mockRestore

--- bloco_10/dia_3-simulacoes-mock/exercices.test.js
+++ bloco_10/dia_3-simulacoes-mock/exercices.test.js
@@ -12,12 +12,22 @@ describe('exercices - jest mock', () => {
         expect(exercices.randomNumber()).toBe(10);
         expect(exercices.randomNumber).toHaveBeenCalledTimes(2);
     })
-
+    
     test('exercicio 2 - randomNumbers mock', () => {
+        expect.assertions(3);
+        exercices.randomNumber.mockClear();
         exercices.randomNumber.mockImplementation((a, b) => a/b);
         expect(exercices.randomNumber(10, 2)).toBe(5);
         expect(exercices.randomNumber(6, 3)).toBe(2);
-        expect(exercices.randomNumber).toHaveBeenCalledTimes(4);
+        expect(exercices.randomNumber).toHaveBeenCalledTimes(2);
     })
-
+    
+    test('exercício 3 - randomNumbers mock reset', () => {
+        exercices.randomNumber.mockImplementation((a, b, c) => a * b * c);
+        expect(exercices.randomNumber(1, 2, 3)).toBe(6);
+        expect(exercices.randomNumber).toHaveBeenCalledWith(1, 2, 3);
+        exercices.randomNumber.mockRestore();
+        expect(exercices.randomNumber()).toBe(50);
+    })
+    
 })
