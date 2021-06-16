aa6695a29e9392c03c20b88ec38466d9fdd196de
RyanLaiber <ryanlaiber@gmail.com>
2021-06-16 19:39:24 -0300

    adiciona exercicio 2, dia 3 bloco 10

--- bloco_10/dia_3-simulacoes-mock/exercices.test.js
+++ bloco_10/dia_3-simulacoes-mock/exercices.test.js
@@ -1,16 +1,23 @@
 const exercices = require('./exercices');
 const { test, describe } = require('@jest/globals');
+jest.mock('./exercices');
 
 describe('exercices - jest mock', () => {
     
     test('exercício 1 - randomNumber', () => {
         expect.assertions(3);
-        exercices.randomNumber = jest.fn()
-            .mockReturnValue(10);
+        exercices.randomNumber.mockReturnValue(10);
         exercices.randomNumber();
         expect(exercices.randomNumber).toHaveBeenCalled();
         expect(exercices.randomNumber()).toBe(10);
         expect(exercices.randomNumber).toHaveBeenCalledTimes(2);
+    })
+
+    test('exercicio 2 - randomNumbers mock', () => {
+        exercices.randomNumber.mockImplementation((a, b) => a/b);
+        expect(exercices.randomNumber(10, 2)).toBe(5);
+        expect(exercices.randomNumber(6, 3)).toBe(2);
+        expect(exercices.randomNumber).toHaveBeenCalledTimes(4);
     })
 
 })
